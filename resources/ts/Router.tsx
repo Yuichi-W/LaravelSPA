46ce526a31f170f4e5d63f4275f2b6c49eae4bef
y.wada <wada1257@gmail.com>
2023-02-26 21:39:59 +0900

    Viewページの作成

---
+++ resources/ts/Router.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { TaskPage } from "./pages/tasks";
+import { LoginPage } from "./pages/login";
+import { HelpPage } from "./pages/help";
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <header className="global-head">
+                <ul>
+                    <li>
+                        <Link to="/">ホーム</Link>
+                    </li>
+                    <li>
+                        <Link to="/help">ヘルプ</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">ログイン</Link>
+                    </li>
+                    <li>
+                        <span>ログアウト</span>
+                    </li>
+                </ul>
+            </header>
+            {/* <h1 className='text-red'>Laravel React+Typescript環境構築</h1> */}
+            <Routes>
+                <Route path={`/`} element={<TaskPage />} />
+                <Route path={`/login/`} element={<LoginPage />} />
+                <Route path={`/help/`} element={<HelpPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
---
+++ resources/ts/pages/help/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const HelpPage = () => {
+    return (
+        <div className="align-center">
+            <h1>ヘルプ</h1>
+            <p>
+                使い方について
+                <br />
+                このサイトはログインが必要となります
+            </p>
+        </div>
+    );
+}
